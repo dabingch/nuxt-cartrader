2a827d9b9beaa0dab35284191d87297db3aae66b
Hao Chen <hche8093@uni.sydney.edu.au>
2023-03-15 10:54:35 +1100

    Add a custom layout

--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -1,12 +1,8 @@
 <template>
 	<div>
-		<div
-			class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
-		>
-			<div class="mt-32 flex">
-				<CarSideBar />
-				<NuxtPage />
-			</div>
+		<div class="mt-32 flex">
+			<CarSideBar />
+			<NuxtPage />
 		</div>
 	</div>
 </template>
@@ -18,6 +14,10 @@ useHead({
 	title: `${
 		route.params.make ? toTitleCase(route.params.make) : 'Cars'
 	} in ${toTitleCase(route.params.city)}`,
+})
+
+definePageMeta({
+	layout: 'custom',
 })
 
 function toTitleCase(str) {
