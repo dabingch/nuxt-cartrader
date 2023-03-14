806f1b495c59a446925a8452135c16fd6f969182
Hao Chen <hche8093@uni.sydney.edu.au>
2023-03-14 12:19:59 +1100

    Filting the page with component

---
+++ components/Car/Card.vue
@@ -0,0 +1,23 @@
+<template>
+	<div
+		class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+	>
+		<div class="flex h-full">
+			<img
+				src="https://images.drive.com.au/driveau/image/upload/c_fill,f_auto,g_auto,h_675,q_auto:eco,w_1200/cms/uploads/pefjmmg5axuxejkk7qsj"
+				alt="Car image"
+				class="w-[300px] h-full"
+			/>
+			<div class="p-4 flex flex-col">
+				<div>
+					<h1 class="text-2xl text-blue-700">Porsche 911 GT3 RS</h1>
+					<p class="text-gray-700">
+						Lorem ipsum dolor sit, amet consectetur adipisicing
+						elit.
+					</p>
+				</div>
+				<h1 class="mt-auto text-xl">$40,000</h1>
+			</div>
+		</div>
+	</div>
+</template>
