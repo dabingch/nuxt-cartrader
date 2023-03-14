58b7ad823bd27c4f4f2d3f1947f3749e6d9b7020
Hao Chen <hche8093@uni.sydney.edu.au>
2023-03-14 12:56:17 +1100

    Change title with useHead

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,13 +1,10 @@
 <template>
 	<div>
-		<head>
-			<title>Cartrader</title>
-		</head>
 		<!-- Navbar -->
 		<header
 			class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
 		>
-			<NuxtLink class="text-3xl font-mono" to="/">cartrader</NuxtLink>
+			<NuxtLink class="text-3xl font-mono" to="/">CarTrader</NuxtLink>
 		</header>
 	</div>
 </template>
--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -11,3 +11,16 @@
 		</div>
 	</div>
 </template>
+
+<script setup>
+const route = useRoute()
+useHead({
+	title: toTitleCase(route.params.name),
+})
+
+function toTitleCase(str) {
+	return str.replace(/\w\S*/g, function (txt) {
+		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+	})
+}
+</script>
