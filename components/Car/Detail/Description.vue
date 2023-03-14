d1cdbc393870ae1d33db29f2c9edd48d884f8101
Hao Chen <hche8093@uni.sydney.edu.au>
2023-03-14 12:28:25 +1100

    Add unique dynamic path

---
+++ components/Car/Detail/Description.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="mt-5">
+		<p class="mb-10">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+			facere expedita ad, id, nostrum soluta, nulla dolor unde atque quae
+			explicabo! Odit illum nisi id praesentium, laudantium fugit numquam
+			ipsa.
+		</p>
+	</div>
+</template>
---
+++ components/Car/Detail/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="mt-10">
+		<img
+			src="https://images.drive.com.au/driveau/image/upload/c_fill,f_auto,g_auto,h_675,q_auto:eco,w_1200/cms/uploads/pefjmmg5axuxejkk7qsj"
+			alt=""
+			class="w-full"
+		/>
+		<h1 class="mt-10 text-4xl">Porsche 911 GT3 RS</h1>
+		<div
+			class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between"
+		>
+			<div class="flex">
+				<p class="mr-2">2 seats</p>
+				<p class="mr-2">|</p>
+				<p class="mr-2">10,000 miles</p>
+			</div>
+			<div>
+				<p class="font-bold text-2xl">$40,000</p>
+			</div>
+		</div>
+	</div>
+</template>
