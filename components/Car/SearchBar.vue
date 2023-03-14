ad4c3eda359dc34bd5377a4aa61f839fcfb7b2f4
Hao Chen <hche8093@uni.sydney.edu.au>
2023-03-14 12:45:14 +1100

    Add nested route

--- components/Car/SearchBar.vue
+++ components/Car/SearchBar.vue
@@ -5,12 +5,27 @@
 		<input
 			type="text"
 			class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
+			:class="cityError ? 'border-red-500 border' : ''"
 			placeholder="Search by city..."
+			v-model="city"
 		/>
-		<button class="bg-sky-500 px-10 text-white">Search</button>
+		<button class="bg-sky-500 px-10 text-white" @click="onSearch">
+			Search
+		</button>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+const city = ref('')
+const cityError = ref(false)
+
+const onSearch = () => {
+	if (!city.value) {
+		cityError.value = true
+		return
+	}
+	navigateTo(`/city/${city.value}/car`)
+}
+</script>
 
 <style lang="scss" scoped></style>
