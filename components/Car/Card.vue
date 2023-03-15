1f39d26f60d30f5f207139416644aa6ea410dc3a
Hao Chen <hche8093@uni.sydney.edu.au>
2023-03-15 11:58:59 +1100

    Add error page and dynamic card component

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,23 +1,25 @@
 <template>
 	<div
 		class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+		@click="navigateTo(`/car/${car.name}-${car.id}`)"
 	>
 		<div class="flex h-full">
-			<img
-				src="https://images.drive.com.au/driveau/image/upload/c_fill,f_auto,g_auto,h_675,q_auto:eco,w_1200/cms/uploads/pefjmmg5axuxejkk7qsj"
-				alt="Car image"
-				class="w-[300px] h-full"
-			/>
+			<img :src="car.url" alt="Car image" class="w-[300px] h-full" />
 			<div class="p-4 flex flex-col">
 				<div>
-					<h1 class="text-2xl text-blue-700">Porsche 911 GT3 RS</h1>
+					<h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
 					<p class="text-gray-700">
-						Lorem ipsum dolor sit, amet consectetur adipisicing
-						elit.
+						{{ car.description }}
 					</p>
 				</div>
-				<h1 class="mt-auto text-xl">$40,000</h1>
+				<h1 class="mt-auto text-xl">${{ car.price }}</h1>
 			</div>
 		</div>
 	</div>
 </template>
+
+<script setup>
+const props = defineProps({
+	car: Object,
+})
+</script>
---
+++ error.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="flex flex-col justify-center h-screen items-center">
+		<h1 class="text-9xl">{{ error.statusCode }}</h1>
+		<p class="mt-7 text-4xl">{{ error.message }}</p>
+		<button
+			@click="onClick"
+			class="rounded mt-7 text-2xl bg-blue-400 px-7 py-4 text-white"
+		>
+			Go Back
+		</button>
+	</div>
+</template>
+
+<script setup>
+const error = useError()
+
+const onClick = () => {
+	navigateTo('/')
+}
+</script>
