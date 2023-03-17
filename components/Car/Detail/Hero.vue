1fc86b258781afc73f85feb8406c1d54230085a0
Hao Chen <hche8093@uni.sydney.edu.au>
2023-03-17 17:15:09 +1100

    Add nuxt image module

--- components/Car/Detail/Hero.vue
+++ components/Car/Detail/Hero.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="mt-10">
-		<img :src="car.url" alt="" class="w-full" />
+		<nuxt-img :src="car.url" alt="" class="w-full" />
 		<h1 class="mt-10 text-4xl">{{ car.name }}</h1>
 		<div
 			class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between"
