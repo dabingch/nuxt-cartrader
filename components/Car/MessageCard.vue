0a0a47307f280efb47edfa33253212f15a2b0336
Hao Chen <hche8093@uni.sydney.edu.au>
2023-03-19 23:00:15 +1100

    Add listing view and message card

---
+++ components/Car/MessageCard.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="even:bg-gray-100 p-4">
+		<h1 class="font-bold text-2xl">Hello World</h1>
+		<p class="text-gray-700">
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
+			iure eius dolore odit quidem expedita, est sed earum minus, porro
+			minima. Illum repudiandae vitae eaque ex. Eos quia asperiores
+			voluptatum eum alias reprehenderit rem tenetur ratione minus,
+			dolorum, ab illo necessitatibus! Assumenda aspernatur id delectus
+			dicta alias officiis ullam sequi!
+		</p>
+		<div class="flex items-center mt-4">
+			<p class="text-green-700 mr-10">dabing@mail.com</p>
+			<p class="text-blue-500">555-555-555</p>
+		</div>
+	</div>
+</template>
---
+++ pages/profile/listings/view/[id].vue
@@ -0,0 +1,3 @@
+<template>
+	<div class="rounded shadow mt-20"></div>
+</template>
