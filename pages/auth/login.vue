fb491f248248fa6b23b636c0c521b3329ae0df1e
Hao Chen <hche8093@uni.sydney.edu.au>
2023-03-21 11:46:15 +1100

    Add google oauth

---
+++ pages/auth/login.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="mt-10">
+		{{ user }}
+		<h1 class="text-5xl font-bold mb-7">Log in</h1>
+		<button
+			@click="handleLogin"
+			class="bg-red-400 p-3 rounded text-white font-bold"
+		>
+			Login with Google
+		</button>
+	</div>
+</template>
+
+<script setup>
+definePageMeta({
+	layout: 'custom',
+})
+
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+
+const handleLogin = async () => {
+	const { error } = await supabase.auth.signInWithOAuth({
+		provider: 'google',
+	})
+
+	if (error) {
+		console.log('Error logging in', error)
+	}
+}
+</script>
