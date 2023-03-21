40835434c961c5dc39b9bb65678a216e62ed829a
Hao Chen <hche8093@uni.sydney.edu.au>
2023-03-21 11:57:51 +1100

    Add login and logout button on Navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,11 +4,32 @@
 		<header
 			class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
 		>
-			<NuxtLink class="text-3xl font-mono" to="/">CarTrader</NuxtLink>
+			<nuxt-link class="text-3xl font-mono" to="/">CarTrader</nuxt-link>
+			<div class="flex" v-if="user">
+				<nuxt-link class="mr-5" to="/profile/listings"
+					>profile</nuxt-link
+				>
+				<p @click="handleLogout" class="cursor-pointer">Logout</p>
+			</div>
+			<nuxt-link v-else to="/auth/login">Login</nuxt-link>
 		</header>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+const user = useSupabaseUser()
+const supabase = useSupabaseClient()
+
+const handleLogout = async () => {
+	const { error } = await supabase.auth.signOut()
+
+	if (error) {
+		console.log('Error logging out', error)
+		return
+	}
+
+	navigateTo('/')
+}
+</script>
 
 <style lang="scss" scoped></style>
