0b6deba44dc6af6c2ac9c67838e9e4b5a5c12ced
Hao Chen <hche8093@uni.sydney.edu.au>
2023-03-24 16:07:47 +1100

    Create a server endpoint for filtering cars

---
+++ server/api/cars/[city].js
@@ -0,0 +1,30 @@
+import cars from '@/data/cars.json'
+
+export default defineEventHandler((event) => {
+	const { city } = event.context.params
+	const { make, minPrice, maxPrice } = getQuery(event)
+
+	let filteredCars = cars.filter(
+		(car) => car.city.toLowerCase() === city.toLowerCase()
+	)
+
+	if (make) {
+		filteredCars = filteredCars.filter(
+			(car) => car.make.toLowerCase() === make.toLowerCase()
+		)
+	}
+
+	if (minPrice) {
+		filteredCars = filteredCars.filter(
+			(car) => car.price >= parseInt(minPrice)
+		)
+	}
+
+	if (maxPrice) {
+		filteredCars = filteredCars.filter(
+			(car) => car.price <= parseInt(maxPrice)
+		)
+	}
+
+	return filteredCars
+})
