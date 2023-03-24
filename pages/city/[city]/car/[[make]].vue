7a02461d651dc34f6cc2acbff99f8602cd75dd81
Hao Chen <hche8093@uni.sydney.edu.au>
2023-03-24 16:33:50 +1100

    Add fetching data from client

--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -1,5 +1,20 @@
 <template>
 	<div>
-		<CarCards />
+		<CarCards :cars="cars" />
 	</div>
 </template>
+
+<script setup>
+const route = useRoute()
+const { data: cars, refresh } = await useFetchCars(route.params.city, {
+	minPrice: route.query.minPrice,
+	maxPrice: route.query.maxPrice,
+	make: route.params.make,
+})
+
+watch(
+	() => route.query,
+	// () => refresh // not work
+	() => window.location.reload(true)
+)
+</script>
